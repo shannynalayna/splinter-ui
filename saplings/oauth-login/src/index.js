b7a511dd57411170d765b642505c122a474d3e03
Shannyn Telander <telander@bitwise.io>
2020-11-05 18:15:44 -0600

    Handle the OAuth access token
    Adds handling for the OAuth access token returned after a user
    logs in successfully. Also adds handling to verify the rendering of the
    login page.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- saplings/oauth-login/src/index.js
+++ saplings/oauth-login/src/index.js
@@ -17,7 +17,8 @@
 import {
   registerConfigSapling,
   registerApp,
-  hideCanopy
+  hideCanopy,
+  setUser
 } from 'splinter-saplingjs';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -25,7 +26,28 @@ import './index.css';
 import App from './App';
 
 registerConfigSapling('oauth-login', () => {
-  if (window.location.pathname === '/oauth-login') {
+  const canopyURL = window.location.href;
+
+  if (canopyURL.includes('access_token')) {
+    // Extract user token information
+    const accessTokenRegex = /\?access_token=([^&#]*)|&|$/;
+    const accessToken = canopyURL.match(accessTokenRegex)[1];
+    const oauthPrefix = 'OAuth2:';
+    // Extract user information
+    const userIdRegex = /\?user_id=([^&#]*)|&|$/;
+    const userId = canopyURL.match(userIdRegex)[1];
+    const displayNameRegex = /\?expires_in=([^&#]*)|&|$/;
+    const displayName = canopyURL.match(displayNameRegex)[1];
+    // Set Canopy user
+    setUser({
+      token: oauthPrefix.concat(accessToken),
+      userId,
+      displayName
+    });
+    const redirectedFrom = window.$CANOPY.redirectedFrom || '/';
+    window.$CANOPY.redirectedFrom = undefined;
+    window.location.href = redirectedFrom;
+  } else if (window.location.pathname === '/oauth-login') {
     hideCanopy();
     registerApp(domNode => {
       ReactDOM.render(<App />, domNode);
